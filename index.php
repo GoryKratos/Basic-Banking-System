fec980c520d717f32733639f5beea934a742b2fa
GoryKratos <kakadsarthak25@gmail.com>
2021-06-21 22:21:24 +0530

    icon added

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Sparks Bank</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    
+    <link rel="icon" href="./styles/icon.png">
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link
